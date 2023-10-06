eb9678bc347b098331529daaa9f46ac675a9ff87
David Moreno Fernandez <dmoreno2@seidor.es>
2023-10-06 13:14:28 +0200

    Stats-3 done

--- estadisticas.py
+++ estadisticas.py
@@ -26,7 +26,38 @@ def jugador_menys_cm(enunciat,dict):
     nom = dict[player_num]["Nom"]
     print("El jugador amb menys cm de la llista es", nom, "amb", cm_petit,"cm")
 
+def mitjana_pes_alçada_equips(enunciat, dict):
+    print(enunciat)
+    new_dict={}
+    for player in dict:
+        actual_equip= ""
+        for clave, values in dict[player].items():
+            if "Equip" == clave and values not in new_dict.keys():
+                new_dict[values]= {"Pes": [], "Alçada": []}
+                actual_equip = values
+            if "Equip" == clave:
+                actual_equip = values
+            elif "Pes" == clave:
+                new_dict[actual_equip]["Pes"].append(values)
+            elif "Alçada" == clave:
+                new_dict[actual_equip]["Alçada"].append(values)
+
+    for equip in new_dict:
+        for clave, values in new_dict[equip].items():
+            if "Pes" == clave:
+                pes = 0.0
+                for valors in values:
+                    pes += valors
+                pes = pes / (len(values) + 1)
+                print("La mitjana de pes de l'equip", equip, "es",round(pes,2),"kg")
+            elif "Alçada" == clave:
+                alçada = 0.0
+                for valors in values:
+                    alçada += valors
+                alçada = alçada / (len(values) + 1)
+                print("La mitjana de cm de l'equip", equip, "es",round(alçada,2),"cm")
 
 def estadisticas(dict,enunciats):
     jugador_mes_pes(enunciats[0],dict)
     jugador_menys_cm(enunciats[1], dict)
+    mitjana_pes_alçada_equips(enunciats[2], dict)
